86eeae0ea2d235e5abc59858f37cb081e8929d89
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-10-28 22:43:40 -0300

    corrected starting proyect
    chose Web App instead of Web Api

---
+++ BackEnd/TP-GSC-BackEnd/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP_GSC_BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult getCategories() => Ok(new List<int> { 1, 2, 3 });
+
+    }
+}
---
+++ BackEnd/TP-GSC-BackEnd/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP_GSC_BackEnd.Entities
+{
+
+    public class Category 
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public Category(string desc) { 
+            this.Description = desc;
+        }
+
+    }
+}
