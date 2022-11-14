b3322b78ea68e14bbfcb18c24d455a4ccad7cdba
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-11-14 17:58:57 -0300

    create thing mvc

--- BackEnd/TP-GSC-BackEnd/Controllers/MVC/ThingsController.cs
+++ BackEnd/TP-GSC-BackEnd/Controllers/MVC/ThingsController.cs
@@ -1,17 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TP_GSC_BackEnd.Data_Access.CategoryData;
+using TP_GSC_BackEnd.Data_Access.Uow;
+using TP_GSC_BackEnd.Dto.CategotyDto;
+using TP_GSC_BackEnd.Dto.ThingDto;
+using TP_GSC_BackEnd.Entities;
+using TP_GSC_BackEnd.Models;
 
 namespace TP_GSC_BackEnd.Controllers.MVC
 {
     public class ThingsController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+        private readonly IUnitOfWork Uow;
+        private readonly IMapper Mapper;
+        public ThingsController(IUnitOfWork uow, IMapper mapper) { 
+            this.Uow = uow;
+            this.Mapper = mapper;
         }
 
-        public IActionResult List() 
+
+        public IActionResult Index()
         {
-            return View();
+            var AllThings = Uow.CategoryRepo.GetAll();
+            var AllThingsDto = Mapper.Map<List<ShowThingDto>>(AllThings);
+            return View(AllThingsDto); ;
+        }
+
+
+
+
+        public IActionResult Create() {
+            var AllCategories = Uow.CategoryRepo.GetAll();
+            var AllCategoriesDto = Mapper.Map<ShowCategoryDto[]>(AllCategories);
+
+            var createThingViewModel = new CreateThingViewModel()
+            {
+                Categories = AllCategoriesDto.ToList(),
+            };
+
+            return View(createThingViewModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreateThingViewModel createThingViewModel) {
+            if (!ModelState.IsValid)
+                return View("Create", createThingViewModel);
+
+            
+            var selectedCategory = Uow.CategoryRepo.GetOne(createThingViewModel.CategoryId);
+            if (selectedCategory is null)
+                return View("Create", createThingViewModel);
+
+            var newThing = new Thing();
+            newThing.Description = createThingViewModel.Description;
+            newThing.Category = selectedCategory;
+
+
+            var createdThing = Uow.ThingsRepo.add(newThing);
+            Uow.SaveChanges();
+
+            return Redirect(nameof(Index));
         }
 
     }
---
+++ BackEnd/TP-GSC-BackEnd/Models/CreateThingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using TP_GSC_BackEnd.Dto.CategotyDto;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Win32.SafeHandles;
+
+namespace TP_GSC_BackEnd.Models
+{
+    public class CreateThingViewModel
+    {
+
+        public List<ShowCategoryDto> Categories=new List<ShowCategoryDto>();
+
+
+        [Required(ErrorMessage = "Description is requiered")]
+        [MinLength(3,ErrorMessage ="Should have more then 3 letters")]
+        [MaxLength(99, ErrorMessage = "Can't have more then 100 letterns")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Category is requiered")]
+        public int CategoryId { get; set; }
+
+    }
+}
